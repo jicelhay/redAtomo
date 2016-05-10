ef00a17ca4df3435dd048179709d11d6784a2233
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-10 00:46:00 -0300

    basic modal test

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,12 @@
+$(document).on('page:change', function() {
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      //ready: function() { alert('Ready'); }, // Callback for Modal open
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+ });
