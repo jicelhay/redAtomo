98f05ac0f354789b5453b7fe87ec0f8df81a28f2
Diego <difernandez@uc.cl>
2016-06-15 00:10:41 -0300

    Small bug fix on authorize_post!

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527041626) do
+ActiveRecord::Schema.define(version: 20160613221220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "media", force: :cascade do |t|
     t.string   "title"
@@ -90,7 +97,7 @@ ActiveRecord::Schema.define(version: 20160527041626) do
     t.string   "authentication_token",   limit: 30
     t.integer  "aula_id"
   end
-  
+
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
