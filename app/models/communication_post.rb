15bfb66897b6231d286a9fffb0a978106be76497
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-06-22 17:30:58 -0400

    quick fix

--- app/models/communication_post.rb
+++ app/models/communication_post.rb
@@ -1,14 +1,10 @@
 class CommunicationPost < ActiveRecord::Base
   belongs_to :user
   belongs_to :school_class
-  
+
   has_attached_file :thumbnail, styles: { medium: '300x300>', thumb: '100x100>' }
   validates_attachment :thumbnail, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
 
   has_attached_file :pdf
   validates_attachment :pdf, content_type: { content_type: %w(application/pdf application) }
-
-  def get_temp_file_path
-    attachment.queued_for_write[:pdf].path
-  end
 end
